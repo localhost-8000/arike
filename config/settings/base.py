06d200e3dd0cebf4e293a8443a078f182177f106
Rahul Tiwari <rahultwr0005@gmail.com>
2022-03-06 18:30:17 +0530

    made changes for tailwind css to deploy on heroku

--- config/settings/base.py
+++ config/settings/base.py
@@ -96,7 +96,7 @@ LOCAL_APPS = [
     "arike.visits"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+INSTALLED_APPS = LOCAL_APPS + DJANGO_APPS + THIRD_PARTY_APPS
 
 # MIGRATIONS
 # ------------------------------------------------------------------------------
