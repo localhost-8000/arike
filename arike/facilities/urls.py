e14900ef1bc0932a9d33e8bcb1a3d163c890ac3f
Rahul Tiwari <rahultwr0005@gmail.com>
2022-03-06 19:36:36 +0530

    fixed url bug in facilities view

--- arike/facilities/urls.py
+++ arike/facilities/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 
-from arike.facilities.views import GenericFacilitiesView, GenericFacilityCreateView, GenericFacilityDeleteView, GenericFacilityDetailView, GenericFacilityUpdateView
-
+from arike.facilities.views import (
+    GenericFacilitiesView,
+    GenericFacilityCreateView,
+    GenericFacilityDeleteView,
+    GenericFacilityDetailView,
+    GenericFacilityUpdateView,
+)
 
 app_name = "facilities"
 
 urlpatterns = [
     path("", GenericFacilitiesView.as_view(), name="facilities"),
     path("create/", GenericFacilityCreateView.as_view(), name="create_facility"),
-    path("<pk>", GenericFacilityDetailView.as_view(), name="facility_detail"),
+    path("detail/<pk>", GenericFacilityDetailView.as_view(), name="facility_detail"),
     path("update/<pk>", GenericFacilityUpdateView.as_view(), name="update_facility"),
     path("delete/<pk>", GenericFacilityDeleteView.as_view(), name="delete_facility"),
 ]
